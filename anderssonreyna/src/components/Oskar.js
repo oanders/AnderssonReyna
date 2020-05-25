24aa77a0d0da1bdf8154154f301ccdcc604b5000
Oskar Andersson <oanders@kth.se>
2020-05-25 20:48:57 +0200

    added page not found

--- anderssonreyna/src/components/Oskar.js
+++ anderssonreyna/src/components/Oskar.js
@@ -4,56 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function Oskar() {
   return (
     <div class="container">
-      <div class="jumbotron text-center">
-        Oskar är 29 år gammal. Lorem ipsum dolor sit amet, consectetur
-        adipiscing elit. Integer arcu odio, tempus nec purus eget, sodales
-        pellentesque elit. Nullam sagittis vel lectus eget tincidunt.
-        Suspendisse tempus semper velit, id scelerisque est ultricies in.
-        Praesent ut efficitur enim. Fusce augue orci, accumsan feugiat eros non,
-        pulvinar pellentesque leo. In hac habitasse platea dictumst. Aenean in
-        mi varius, pulvinar lacus at, sagittis odio. Phasellus tristique ex
-        ante, at vulputate leo tincidunt eget. Quisque suscipit, sapien molestie
-        sollicitudin lacinia, massa turpis porta nunc, vel laoreet urna arcu vel
-        ante. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Suspendisse rhoncus non urna et consequat. In hac habitasse platea
-        dictumst. Proin imperdiet facilisis purus, ac semper ipsum fringilla
-        eget. Vivamus a justo ut dui vehicula euismod. Quisque mattis arcu
-        ligula, id volutpat nulla sollicitudin at. Fusce aliquet non turpis sit
-        amet faucibus. Duis ultrices ante mi, in elementum mi lacinia eget.
-        Donec justo justo, molestie nec dui tristique, vulputate lobortis arcu.
-        Aenean porta tellus quis pellentesque imperdiet. Nulla convallis sit
-        amet ipsum non faucibus. Suspendisse ac ex at ipsum blandit semper.
-        Etiam pharetra purus at feugiat placerat. Phasellus cursus, leo quis
-        vulputate vulputate, erat sem venenatis lacus, sed commodo arcu metus
-        quis velit. Quisque rutrum volutpat nisi, ac suscipit risus. Vivamus et
-        justo a eros fringilla tempus sit amet non ipsum. Suspendisse potenti.
-        Integer ultricies accumsan nibh a tincidunt. Aliquam eget est gravida,
-        fringilla purus eget, lobortis purus. Nullam quis leo ultrices,
-        malesuada urna a, ornare nisi. Integer placerat mattis nulla, ac
-        placerat nisi egestas id. Suspendisse libero lorem, venenatis sit amet
-        suscipit a, faucibus et metus. Donec laoreet fringilla est, venenatis
-        dictum sem rutrum sed. Nulla pharetra tellus ut mauris congue venenatis.
-        Cras nec feugiat magna, non accumsan est. Proin rutrum vel leo sit amet
-        mollis. Donec eu faucibus mi. Donec dictum rhoncus elit, ac fermentum
-        orci consectetur in. Etiam eget leo ornare, laoreet est vitae, dapibus
-        sapien. Maecenas cursus erat orci, nec porttitor arcu placerat nec.
-        Aenean sodales eros non leo sollicitudin lacinia. Proin dictum turpis at
-        ipsum ullamcorper fringilla. Morbi eu risus aliquet, lacinia turpis ut,
-        volutpat sem. Donec ac tristique arcu, et sagittis enim. Etiam aliquam
-        nisl id sapien rutrum lobortis. Morbi vestibulum accumsan sem. Lorem
-        ipsum dolor sit amet, consectetur adipiscing elit. Nulla non dui varius,
-        faucibus nibh at, posuere tortor. Aliquam cursus nunc nec rhoncus
-        mollis. Suspendisse suscipit scelerisque risus et aliquet. Duis
-        facilisis mollis dignissim. Maecenas porttitor sem lacinia ornare
-        fermentum. Suspendisse eu finibus arcu. Nullam rutrum, ligula nec
-        posuere malesuada, eros nibh imperdiet felis, eget placerat nunc nisl at
-        justo. Sed et sodales ante. Aliquam erat volutpat. Nulla facilisi. Donec
-        viverra sagittis augue, sit amet consequat turpis sagittis ut. Quisque
-        quis turpis id nisl semper interdum quis nec diam. Lorem ipsum dolor sit
-        amet, consectetur adipiscing elit. Nulla congue tellus dui, non
-        convallis ex ullamcorper at. Vivamus condimentum lorem et porttitor
-        ultrices. Maecenas tincidunt nec massa auctor interdum.{" "}
-      </div>
+      <div class="jumbotron text-center">Oskar är 29 år gammal.</div>
     </div>
   );
 }
