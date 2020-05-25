e303b089c4b7d6b02591d6a28cbef723ac45acb3
Oskar Andersson <oanders@kth.se>
2020-05-25 14:48:05 +0200

    added basic routes in backend

--- dynamic/app.py
+++ dynamic/app.py
@@ -6,7 +6,7 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-USERS_TABLE = os.environ["USERS_TABLE"]
+GUESTS_TABLE = os.environ["GUESTS_TABLE"]
 client = boto3.client("dynamodb")
 
 IS_OFFLINE = os.environ.get("IS_OFFLINE")
@@ -26,30 +26,75 @@ def hello():
     return "Hello World!"
 
 
-@app.route("/users/<string:user_id>")
-def get_user(user_id):
+@app.route("/guests/<string:guest_id>")
+def get_guest(guest_id):
 
-    resp = client.get_item(TableName=USERS_TABLE, Key={"userId": {"S": user_id}})
+    resp = client.get_item(TableName=GUESTS_TABLE, Key={"guestId": {"S": guest_id}})
 
     item = resp.get("Item")
 
     if not item:
-
-        return jsonify({"error": "User does not exist"}), 404
+        return jsonify({"error": "Guest does not exist"}), 404
     return jsonify(
-        {"userId": item.get("userId").get("S"), "name": item.get("name").get("S")}
+        {
+            "name": item.get("name").get("S"),
+            "attending": item.get("attending").get("S"),
+            "additionals": item.get("additionals").get("N"),
+        }
     )
 
 
-@app.route("/users", methods=["POST"])
-def create_user():
-    user_id = request.json.get("userId")
-    name = request.json.get("name")
-    if not user_id or not name:
-        return jsonify({"error": "Please provide userId and name"}), 400
+@app.route("/guests")
+def list_guests():
+    resp = client.scan(TableName=GUESTS_TABLE)
+    guests = []
+    for item in resp["Items"]:
+        guests.append(
+            {
+                "name": item.get("name").get("S"),
+                "attending": item.get("attending").get("S"),
+                "additionals": item.get("additionals").get("N"),
+            }
+        )
+    return jsonify({"guests": guests})
+
+
+@app.route("/guests", methods=["POST"])
+def create_guest():
+    guest_id = request.json.get("guestId")
+    email = request.json.get("email")
+    name = request.json.get("fullName")
+    address = request.json.get("address")
+    phone = request.json.get("phone")
+    attending = request.json.get("attending")
+    additionals = request.json.get("additionals")
+    if not guest_id or not name or not email or not additionals:
+        return (
+            jsonify({"error": "Please provide guestId, name, email and additionals."}),
+            400,
+        )
 
     resp = client.put_item(
-        TableName=USERS_TABLE, Item={"userId": {"S": user_id}, "name": {"S": name}}
+        TableName=GUESTS_TABLE,
+        Item={
+            "guestId": {"S": guest_id},
+            "email": {"S": email},
+            "name": {"S": name},
+            "address": {"S": address},
+            "phone": {"S": phone},
+            "attending": {"S": attending},
+            "additionals": {"N": additionals},
+        },
     )
 
-    return jsonify({"userId": user_id, "name": name})
+    return jsonify(
+        {
+            "guestId": guest_id,
+            "email": email,
+            "name": name,
+            "address": address,
+            "phone": phone,
+            "attending": attending,
+            "additionals": additionals,
+        }
+    )
